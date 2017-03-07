a1a5ca54857fdae9b6ac1dae0d6dfce2d22a2bc8
Pedro Silva <tagido@gmail.com>
2017-03-07 18:49:16 +0000

    Code beautifying

--- super8tools/scripts/super8_to_mp4.rb
+++ super8tools/scripts/super8_to_mp4.rb
@@ -1,14 +1,31 @@
 #
-# super8_to_mp4.rb
+#   super8_to_mp4.rb
+#   ================
+#   Converts a Super8 telecined video file to an MP4 output format with suitable filters for Super telecine
 #
+#   Copyright (C) 2016 Pedro Mendes da Silva 
+# 
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+# 
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+# 
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #
 
 
 def convert_chapter start_time,end_time,file_index
-	artist="Amy Winehouse"
-	album= "Back to Black"
+	artist="DJ Estaline"
+	album= "Martelos"
 	date=  "2006"
-	genre= "R&B"
+	genre= "HipHop"
 
    #start_time = start_time - 0.25
    #end_time = end_time + 0.25
@@ -29,6 +46,7 @@ def convert_chapter start_time,end_time,file_index
    system "#{conv_command}\n"
 end
 
+# TODO: automate dependencies and directories (currently hardcoded)
 FFMPEG_PATH="D:\\Program Files (x86)\\FFmpeg for Audacity\\"
 TARGET_PATH="G:.\\"
 TARGET_FILENAME=ARGV[0]
